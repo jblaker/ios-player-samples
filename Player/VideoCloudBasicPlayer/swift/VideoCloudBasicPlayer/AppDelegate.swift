6ef9f1eb9af18f1dda1437b0e6231ad862cbaba8
Jeremy Blaker <jblaker@brightcove.com>
2018-11-07 14:02:43 -0500

    Updated some URLs to Apple Documentation

--- Player/VideoCloudBasicPlayer/swift/VideoCloudBasicPlayer/AppDelegate.swift
+++ Player/VideoCloudBasicPlayer/swift/VideoCloudBasicPlayer/AppDelegate.swift
@@ -18,7 +18,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // We need the below code in order to ensure that audio plays back when we
         // expect it to. For example, without setting this code, we won't hear the video
         // when the mute switch is on. For simplicity in the sample, we are going to
-        // put this in the app delegate.  Check out https://developer.apple.com/Library/ios/documentation/Audio/Conceptual/AudioSessionProgrammingGuide/Introduction/Introduction.html
+        // put this in the app delegate.  Check Out https://developer.apple.com/documentation/avfoundation/avaudiosession
+        // and https://developer.apple.com/documentation/avfoundation/avaudiosession/category/1616509-playback
         // for more information on how to use this in your own app.
 
         var categoryError :NSError?
